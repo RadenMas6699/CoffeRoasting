9530f4d7f853b44be4afc2edf2ca1331c6d6a7e0
RadenMas6699 <radenmas.dev@gmail.com>
2022-08-29 16:24:09 +0800

    Penambahan Menu Info Aplikasi

---
+++ app/src/main/java/com/radenmas/cofferoasting/InfoAplikasiActivity.kt
@@ -0,0 +1,11 @@
+package com.radenmas.cofferoasting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoAplikasiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_aplikasi)
+    }
+}
