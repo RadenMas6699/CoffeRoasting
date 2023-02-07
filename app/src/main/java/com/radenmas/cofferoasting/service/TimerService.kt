97731b9d0463fe2538690e036b0d706436647a73
RadenMas6699 <radenmas.dev@gmail.com>
2023-02-07 15:13:05 +0800

    Add Timer

---
+++ app/src/main/java/com/radenmas/cofferoasting/service/TimerService.kt
@@ -0,0 +1,46 @@
+/*
+ * Created by RadenMas on 25/1/2023.
+ * Copyright (c) 2023.
+ */
+
+package com.radenmas.cofferoasting.service
+
+/**
+ * Created by RadenMas on 25/01/2023.
+ */
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import java.util.*
+
+class TimerService : Service() {
+    override fun onBind(p0: Intent?): IBinder? = null
+
+    private val timer = Timer()
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val time = intent.getDoubleExtra(TIME_EXTRA, 0.0)
+        timer.scheduleAtFixedRate(TimeTask(time), 0, 1000)
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        timer.cancel()
+        super.onDestroy()
+    }
+
+    private inner class TimeTask(private var time: Double) : TimerTask() {
+        override fun run() {
+            val intent = Intent(TIMER_UPDATED)
+            time++
+            intent.putExtra(TIME_EXTRA, time)
+            sendBroadcast(intent)
+        }
+    }
+
+    companion object {
+        const val TIMER_UPDATED = "timerUpdated"
+        const val TIME_EXTRA = "timeExtra"
+    }
+
+}
--- app/src/main/java/com/radenmas/cofferoasting/ui/LightRoastActivity.kt
+++ app/src/main/java/com/radenmas/cofferoasting/ui/LightRoastActivity.kt
@@ -5,15 +5,26 @@
 
 package com.radenmas.cofferoasting.ui
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.firebase.messaging.FirebaseMessaging
+import com.radenmas.cofferoasting.R
 import com.radenmas.cofferoasting.databinding.ActivityLightRoastBinding
+import com.radenmas.cofferoasting.service.TimerService
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToInt
 
 class LightRoastActivity : AppCompatActivity() {
 
@@ -22,19 +33,42 @@ class LightRoastActivity : AppCompatActivity() {
     private var requestQueue: RequestQueue? = null
     private var stringData: StringRequest? = null
 
+    private var timerStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityLightRoastBinding.inflate(layoutInflater)
         val view = b.root
         setContentView(view)
 
-        initView()
+        serviceIntent = Intent(this, TimerService::class.java)
+        registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
 
+        initView()
         onClick()
     }
 
     private fun onClick() {
+        b.btnStart.setOnClickListener {
+            val timer = object : CountDownTimer(960000, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    b.tvTime.text = formatClockSimple(millisUntilFinished)
+                }
 
+                override fun onFinish() {
+                    Toast.makeText(this@LightRoastActivity, "DONE", Toast.LENGTH_SHORT).show()
+                }
+            }
+            timer.start()
+
+//            if (timerStarted) {
+//                stopTimer()
+//            } else {
+//                startTimer()
+//            }
+        }
     }
 
     private fun initView() {
@@ -68,5 +102,52 @@ class LightRoastActivity : AppCompatActivity() {
         lalu gas akan mati
         */
 
+    }
+
+    private fun resetTimer() {
+        stopTimer()
+        time = 0.0
+        b.tvTime.text = getTimeStringFromDouble(time)
+    }
+
+    private fun startTimer() {
+        serviceIntent.putExtra(TimerService.TIME_EXTRA, time)
+        startService(serviceIntent)
+        b.btnStart.text = "Stop"
+//        b.btnStart.icon = getDrawable(R.drawable.ic_pause)
+//        timerStarted = true
+    }
+
+    private fun stopTimer() {
+        stopService(serviceIntent)
+        b.btnStart.text = "Start"
+//        b.btnStart.icon = getDrawable(R.drawable.ic_play)
+        timerStarted = false
+    }
+
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(TimerService.TIME_EXTRA, 0.0)
+            b.tvTime.text = getTimeStringFromDouble(time)
+        }
+    }
+
+    private fun getTimeStringFromDouble(time: Double): String {
+        val resultInt = time.roundToInt()
+//        val hours = resultInt % 86400 / 3600
+        val minutes = resultInt % 86400 % 3600 / 60
+        val seconds = resultInt % 86400 % 3600 % 60
+
+        return makeTimeString(minutes, seconds)
+    }
+
+    private fun makeTimeString(min: Int, sec: Int): String = String.format("%02d:%02d", min, sec)
+
+    fun formatClockSimple(timestamp: Long): String {
+        val formatDate = SimpleDateFormat(
+            "mm:ss", Locale("ID")
+        )
+        val date = Date(timestamp)
+        return formatDate.format(date)
     }
 }
